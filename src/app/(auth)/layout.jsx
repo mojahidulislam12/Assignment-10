2ea8d3f1db116d8237a0d7e2e11d362279f5e7de
Sagor <mojahidulislamsagor1234@gmail.com>
2026-06-28 16:02:22 +0600

    Suspense Added

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,12 +1,16 @@
 import Footer from "@/Components/Share/Footer";
 import NavBar from "@/Components/Share/NavBar";
+import { Suspense } from "react";
 import { Toaster } from "react-hot-toast";
 
 export default function RootLayout({ children }) {
   return (
     <div>
       <NavBar></NavBar>
-      <main> {children}</main>
+      <Suspense fallback={<p>Loading.....</p>}>
+        <main> {children}</main>
+      </Suspense>
+
       <Footer></Footer>
       <Toaster />
     </div>
